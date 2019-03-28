fedecf017e0aed633ba4e336127524c8b931c993
8904482776rach <48976759+8904482776rach@users.noreply.github.com>
2019-03-28 13:38:52 +0530

    Update text.c

--- text.c
+++ text.c
@@ -1,5 +1,15 @@
-#inlcude<stdio.h>
+#include<stdio.h>
+#include<stdlib.h>
+#include<assert.h>
+#include "max.h"
+
 int main()
-{ printf("HELLO WORLD");
-  return 0;
+{  //char c;
+
+   assert(max('+',10,15)==25);
+   assert(max('*',10,15)==150);
+   assert(max('-',15,15)==0);
+   assert(max('/',5,2)==2.5);
+   printf("OK");
+   return 0;
 }
